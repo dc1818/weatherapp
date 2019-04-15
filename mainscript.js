e483de36f94d5a3c514268ff5b9a961c3e26087d
dc1818 <dalton.cornwell@gmail.com>
2019-04-15 17:09:32 -0400

    main code completed need to add seperate page display functions

--- mainscript.js
+++ mainscript.js
@@ -4,6 +4,9 @@ const textbox = document.getElementById('cityinput');
 const form = document.getElementById('form');
 const entirepage = document.getElementById('entirepage');
 const geocodekey  = "UnKAjeHAB6sFQGrli30eJCYQis96tuaO";
+const head = document.getElementsByTagName('head')[0];
+let stylesheet = document.styleSheets[0];
+
 let address;
 const websitetemplate = `http://www.mapquestapi.com/geocoding/v1/address?key=${geocodekey}`;
 const weathertemplate = "https://api.weather.gov/points/";
@@ -15,33 +18,88 @@ async function runGeoCodeApi()
 {
   const response = await fetch(websitetemplate+`&location=${address}`);
   const data = await response.json();
+  const locationData = await data.results[0].locations[0]
+console.log(locationData);
 
-console.log(data);
+lat = locationData.displayLatLng.lat;
+long = locationData.displayLatLng.lng;
 
-lat = data.results[0].locations[0].displayLatLng.lat;
-long = data.results[0].locations[0].displayLatLng.lng;
-
-runWeatherApi();
+runWeatherApi(locationData);
 
 }
-async function runWeatherApi()
+async function runWeatherApi(location)
 {
-  const response = await fetch(weathertemplate+lat+","+long);
-  const data = await response.json();
-  console.log(data);
+  const initialapi = await fetch(weathertemplate+lat+","+long);
+  const initialapidata = await initialapi.json();
+
+
+  const forecasturl = initialapidata.properties.forecast;
+  const forecastapi = await fetch(forecasturl);
+  const forecastdata = await forecastapi.json();
+  const period = await forecastdata.properties.periods[0];
+
+  pickApartForecast(period,location);
+
 }
+  async function pickApartForecast(forecast,locationData)
+  {
+      //const location = await reverseGeoCode(coords);
+        const country = locationData.adminArea1;
+        const state = locationData.adminArea3;
+        const county = locationData.adminArea4;
+        const city = locationData.adminArea5;
+        const street = locationData.street;
+        const zipcode = locationData.postalCode;
+
+
+        let locations = {street:street,city:city,state:state,zipcode:zipcode,county:county,country:country};
+
+
+    console.log(street);
+
+
+  const shortForecast = forecast.shortForecast;
+  //cssSorter("sunny");
+  displayHTML(shortForecast,locations);
+
+
+}
+async function reverseGeoCode(geocode)
+{
+  const reverseGeoCodeApi = await fetch(`http://open.mapquestapi.com/geocoding/v1/reverse?key=${geocodekey}&location=${geocode[1]},${geocode[0]}&includeRoadMetadata=true&includeNearestIntersection=true`);
+  const reverseGeoData = await reverseGeoCodeApi.json();
+  console.log(reverseGeoData);
+  return await reverseGeoData;
+}
+
+function cssSorter(data)
+{
+  if(data ==="mostly sunny")
+  {
+
+  }
+  else if(data ==="sunny")
+  {
+      console.log("load sunny.css");
+      loadCSSFile("sunny.css")
+  }
+}
+
+
 function variableFontSize()
 {
-  if(textbox.value.length<20)
-  {
-
-  }
-  else if(textbox.value.length>=20)
+  if(textbox.value.length>=20)
   {
     textbox.style.fontSize = "400%";
-
   }
+}
+function displayHTML(limitedForecast,locations)
+{
+  //let cloud = "<div class = "cloudbase"><div class = "cloudleft"></div><div class = "cloudright"></div></div";
 
+
+
+  document.body.innerHTML = `<h2>Your limited forecast in ${locations.city} is ${limitedForecast}</h2>`;
 }
 function handler(e)
 {
@@ -59,6 +117,17 @@ function hideLabel()
   citylabel.style.opacity = 0;
   console.log('hidelabel');
 }
+function loadCSSFile(filename)
+{
+  stylesheet.disabled=true;
+  let fileref = document.createElement('link');
+  fileref.setAttribute('rel',"stylesheet");
+  fileref.setAttribute('type','text/css');
+  fileref.setAttribute('href',"./css/"+filename);
+  head.appendChild(fileref);
+  console.log(filename);
+}
+
 
 window.onload = function()
 {
@@ -75,9 +144,12 @@ document.addEventListener('keydown',(e)=>
     document.body.innerHTML = "";
     runGeoCodeApi();
   }
-  else{
+  else if(e.keyCode!=13)
+  {
     variableFontSize();
   }
+
+
 });
 };
 
